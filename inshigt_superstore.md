2f1d091945c6f5e352a1e50033d66faee4ca4abd
WahyuWahid14 <wahyuwahid146@gmail.com>
2025-07-21 13:17:07 +0700

    push script ecomers analyst with sql

---
+++ inshigt_superstore.md
@@ -0,0 +1,4 @@
+TOTAL SALES PER KATEGORI
+
+1. Performa Technology: " Technology adalah category penjualan barang tebanyak, Produk teknologi (seperti laptop atau aksesori elektronik lain) mungkin memiliki permintaan tinggi atau harga per unit lebih mahal dibanding kategori lain."
+2. Performa Kategori Furniture vs Office Supplies: "Furniture mungkin memiliki margin keuntungan lebih besar, tetapi Office Supplies bisa memiliki volume transaksi lebih tinggi karena produknya lebih sering dibeli (contoh: kertas, pulpen)."
---
+++ query_mini_project_sql.sql
@@ -0,0 +1,43 @@
+USE mini_project_sql;
+
+SELECT * FROM super_store;
+
+--total sales per kategory
+SELECT Category, SUM(Sales) AS total_sales FROM super_store GROUP BY Category ORDER BY total_sales DESC;
+
+--rata-rata profit per region
+SELECT region, AVG(profit) AS profit FROM super_store GROUP BY region;
+
+SELECT customer_id, customer_name, SUM(sales) AS total_sales FROM super_store GROUP BY customer_name ORDER BY total_sales DESC;
+
+--case statment untuk klasifikasi peforma penjualan
+SELECT 
+    YEAR(STR_TO_DATE(`order_date`, '%m/%d/%Y')) AS tahun,
+    CONCAT('Q', QUARTER(STR_TO_DATE(`order_date`, '%m/%d/%Y'))) AS kuartal,
+    SUM(sales) AS total_sales
+FROM super_store
+GROUP BY tahun, kuartal
+ORDER BY tahun, kuartal;
+
+
+
+
+
+
+
+
+
+--case statment untuk klasifikasi performa penjualan
+SELECT 
+    customer_id, 
+    customer_name, 
+    SUM(sales) AS total_sales,
+    CASE 
+        WHEN SUM(sales) < 230 THEN 'Rendah'
+        WHEN SUM(sales) >= 230 AND SUM(sales) < 22638 THEN 'Sedang'
+        WHEN SUM(sales) >= 22638 THEN 'Tinggi'
+    END AS performa_penjualan
+FROM super_store
+GROUP BY customer_id, customer_name
+ORDER BY total_sales DESC;
+
